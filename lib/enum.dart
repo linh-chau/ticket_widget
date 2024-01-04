fcd02af747e8d686cd938a6523ea90470728ea9f
linh_tomosia <linh.chau@tomosia.com>
2024-01-04 09:40:24 +0700

    add repo

---
+++ lib/enum.dart
@@ -0,0 +1,5 @@
+enum CardType {
+  image,
+}
+
+enum CardPosition { horizontal, vertical }
---
+++ lib/ticket_widget.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:ticket_widget/enum.dart';
+import 'package:ticket_widget/ticket_cliper.dart';
+
+class Ticket extends StatelessWidget {
+  final double margin;
+  final double borderRadius;
+  final double clipRadius;
+  final double smallClipRadius;
+  final int numberOfSmallClips;
+  final CardPosition cardPosition;
+  final Color backgroundColor;
+  final Widget child;
+  final List<BoxShadow>? boxShadow;
+
+  const Ticket({
+    Key? key,
+    this.margin = 20,
+    this.borderRadius = 10,
+    this.clipRadius = 10,
+    this.smallClipRadius = 4,
+    this.numberOfSmallClips = 10,
+    this.cardPosition = CardPosition.horizontal,
+    this.backgroundColor = Colors.blue,
+    this.boxShadow,
+    required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+    final ticketWidth = screenSize.width - margin * 2;
+    final ticketHeight = ticketWidth * 1.02;
+
+    return Container(
+      width: ticketWidth,
+      height: ticketHeight,
+      decoration: BoxDecoration(
+        boxShadow: boxShadow ??
+            [
+              BoxShadow(
+                offset: const Offset(0, 4),
+                color: Colors.black.withOpacity(0.2),
+                blurRadius: 5,
+                spreadRadius: 2,
+              ),
+            ],
+      ),
+      child: ClipPath(
+        clipper: TicketClipper(
+          borderRadius: borderRadius,
+          clipRadius: clipRadius,
+          smallClipRadius: smallClipRadius,
+          numberOfSmallClips: numberOfSmallClips,
+          cardPosition: CardPosition.vertical,
+        ),
+        child: Container(
+          color: backgroundColor,
+          child: child,
+        ),
+      ),
+    );
+  }
+}
